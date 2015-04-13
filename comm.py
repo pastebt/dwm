1e43f27c9f85f1fbccb6c5d992634fe5e1bc63c7
pastebt <maye1968@gmail.com>
2015-04-12 23:06:40 -0700

    support gzip

--- comm.py
+++ comm.py
@@ -1,9 +1,14 @@
 import sys
-
-from urllib.request import HTTPCookieProcessor, ProxyHandler
-from urllib.request import HTTPRedirectHandler, Request
-from urllib.request import build_opener
-
+import zlib
+try:
+    from urllib.request import HTTPCookieProcessor, ProxyHandler
+    from urllib.request import HTTPRedirectHandler, Request
+    from urllib.request import build_opener
+except ImportError:
+    from urllib2 import HTTPCookieProcessor, ProxyHandler
+    from urllib2 import HTTPRedirectHandler, Request
+    from urllib2 import build_opener
+    
 
 class DWM(object):
     def __init__(self):
@@ -13,11 +18,30 @@ class DWM(object):
         self.opener = build_opener(self.redirh, self.cookie)
 
     def get_html(self, url):
+        '''
+        Date: Mon, 13 Apr 2015 05:35:05 GMT
+        Content-Type: text/html
+        Transfer-Encoding: chunked
+        Connection: close
+        Server: Tengine
+        Content-Encoding: gzip
+        X-Cache: HIT from us-newyork-ubi.hdslb.com
+        '''
         req = Request(url)
         rep = self.opener.open(req)
-        #print(rep.info())
-        print(rep.read())
+        hds = rep.info()
+        #print(repr(info))
+        data = rep.read()
+        if hds.get("Content-Encoding") == 'gzip':
+            print("It is gzip")
+            html = zlib.decompress(data, zlib.MAX_WBITS|16)
+            return html
+        else:
+            return data
 
 d = DWM()
-d.get_html("http://www.bilibili.com/video/av2060396/")
-
+html = d.get_html("http://www.bilibili.com/video/av2060396/")
+print(html)
+#print(zlib.decompress(open('a.zip', 'rb').read(), -zlib.MAX_WBITS))  # deflate
+#print(zlib.decompress(open('a.zip', 'rb').read(), zlib.MAX_WBITS))   # zlib
+#print(zlib.decompress(open('a.zip', 'rb').read(), zlib.MAX_WBITS|16)) # gzip
