a423989d9872a53fa42d88d3087e581c2e8c5f2b
pastebt <maye1968@gmail.com>
2017-03-06 22:08:04 -0800

    clean

--- tudou.py
+++ tudou.py
@@ -1,7 +1,5 @@
 # -*- coding: utf8 -*-
 
-#from subprocess import Popen, PIPE
-
 from mybs import SelStr
 from comm import DWM, echo, start, match1, U
 
@@ -13,23 +11,14 @@ class TUDOU(DWM):
 
     def query_info(self, url):
         # http://www.tudou.com/albumplay/zgdaPAjRz1s/8cUPFUj8sl4.html
-        #http://8drama.net/ipobar_.php?sign=251438...
-        #echo('phantomjs wait ...')
-        #p = Popen(["./phantomjs", "dwm.js", "300", url], stdout=PIPE)
-        #html = p.stdout.read()
-        #hutf = html.decode('utf8')
-        #p.wait()
-        #url = SelStr('video source', hutf)[0]['src']
-        #title = SelStr('h1.entry-title', hutf)[0].text
-        #return title, None, [url], None
         hutf = self.get_hutf(url)
         vcode = match1(hutf, U("vcode:\s*'([^']+)',\s*lan\:\s*'粤语'"))
         echo("vcode", vcode)
         yu = "http://youku.com/v_show/id_" + vcode
+        #return title, None, [url], None
         return YOUKU().query_info(yu)
 
     def get_playlist(self, url):
-        # "a.item.item_positive"
         ns = SelStr('a.item.item_positive', self.phantom_hutf(url))
         return [(a.text.strip(), a['href']) for a in ns]
 
