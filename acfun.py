937e4233d26c3d7971714aee711aa33caf2fd68c
pastebt <maye1968@gmail.com>
2017-03-25 17:48:11 -0700

    update acfun

--- acfun.py
+++ acfun.py
@@ -39,7 +39,7 @@ def rc4(b, d):
 
 
 class ACFUN(DWM):
-    handle_list = ['\.acfun\.cn/v/']
+    handle_list = ['(\.|/)acfun\.cn/v/']
 
     def query_info(self, url):
         # url = 'http://www.acfun.cn/v/ac3526338'
@@ -69,6 +69,9 @@ class ACFUN(DWM):
         segs = stream.get('segs', [])
         m3u8 = stream.get('m3u8', '')
         styp = stream["stream_type"]
+        l = stream.get('total_size', 0)
+        if l == 0:
+            l = stream.get('size', 0)
         if segs:
             urls = [s['url'] for s in segs]
         elif m3u8:
@@ -76,7 +79,7 @@ class ACFUN(DWM):
             urls = self.try_m3u8(m3u8)
             ext="ts"
         # title, ext, urls, size
-        return title, ext, urls, stream['total_size']
+        return title, ext, urls, l
 
     def sel_stream(self, data, stype="Auto"):
         s = None
@@ -85,8 +88,12 @@ class ACFUN(DWM):
             if d['stream_type'] == stype:
                 s = d
                 break
-            if d['total_size'] > m:
-                m = d['total_size']
+            #if d['total_size'] > m:
+            l = d.get('total_size', 0)
+            if l == 0:
+                l = d.get('size', 0)
+            if l > m:
+                m = l
                 s = d
         debug("stream_type =", s['stream_type'], ", total_size =", s['total_size'])
         return s
@@ -96,7 +103,9 @@ class ACFUN(DWM):
         return [(a.text.strip(), a['href']) for a in ns]
 
     def test(self, args):
-        url = 'http://www.acfun.cn/v/ac3526338'
+        #url = 'http://www.acfun.cn/v/ac3526338'
+        url = 'http://acfun.cn/v/ac3446799'
+        #url = 'http://acfun.cn/v/ac3363968'
         hutf = self.get_hutf(url)
         for s in SelStr("script", hutf):
             t = s.text.strip()
@@ -113,7 +122,7 @@ class ACFUN(DWM):
         '''
         {"encode":"1_1489346126_cd3a0e8575edd448bbd6e497a65908bc","sourceId":"58be74680cf2a0edfd235a75","contentId":3526338,"allowDanmaku":0,"title":"时空线索","userId":10171686,"danmakuId":4938063,"sourceType":"zhuzhan","createTime":"2017-03-07 17:40:59.0","videoList":[{"bitRate":99,"playUrl":"58be74680cf2a0edfd235a75"}],"success":true,"startTime":0,"id":4938063,"time":7565,"config":0,"player":"youku","status":2}
         '''
-        #echo(hutf)
+        echo(hutf)
         """
 curl 'http://aplay-vod.cn-beijing.aliyuncs.com/acfun/web?vid=58be74680cf2a0edfd235a75&ct=85&ev=2&sign=1_1489616951_c07d20643dad14757cfa9aa122a6b33d&time=1489616965963' -H 'Pragma: no-cache' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36' -H 'Accept: */*' -H 'Referer: http://www.acfun.cn/v/ac3526338' -H 'X-Requested-With: ShockwaveFlash/22.0.0.192' -H 'Connection: keep-alive' -H 'Cache-Control: no-cache' --compressed
         """
@@ -124,12 +133,13 @@ curl 'http://aplay-vod.cn-beijing.aliyuncs.com/acfun/web?vid=58be74680cf2a0edfd2
         hutf = self.get_hutf(url)
         #echo(hutf)
         d = json.loads(hutf)
+        echo(d)
         #s = d['data']
         #key = "328f45d8"
         key = "2da3ca9e"
         data = json.loads(rc4(key, base64.b64decode(d['data'])))
-        #import pprint
-        #pprint.PrettyPrinter(indent=4).pprint(data)
+        import pprint
+        pprint.PrettyPrinter(indent=4).pprint(data)
         for s in data['stream']:
             echo(s['stream_type'], s['total_size'], s['resolution'], len(s.get('segs', ['No Segs'])))
 
