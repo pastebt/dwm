19d2aad5630289ca6d77867df45421f78b4f9f27
pastebt <maye1968@gmail.com>
2017-07-23 23:41:57 -0700

    qdrama post

--- qdrama.py
+++ qdrama.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 
+
 from mybs import SelStr
 from dailymotion import DM
 from comm import DWM, echo, start
@@ -28,17 +29,33 @@ class QDRAMA(DWM):
 
 
     def test(self, args):
+        import time
+        from urllib2 import urlopen
+        from urllib import urlencode
+
         #url = "http://qdrama.org/k2/"
         hutf = self.get_hutf(args.url)
         #echo(hutf)
         title = SelStr("div.title.sizing h1", hutf)[0].text
         #echo("title =", title)
         nodes = SelStr("div#playsource a", hutf)
-        cnt = 1
+        cnt = 0
         for node in nodes:
-            echo("%s_%02d" % (title, cnt), node['href'])
             cnt += 1
- 
+            t = "%s_%02d" % (title.encode('utf8'), cnt)
+            u = node['href']
+            if 'daily' not in u:
+                continue
+            echo(t, u)
+            if cnt < 0:
+                continue
+            data = urlencode({"aviurl": u,
+                              "avitil": t,
+                              "destdn": "../dwm/xman/",
+                              "sub": "Start"})
+            urlopen("http://127.0.0.1:8080/", data).read()
+            time.sleep(2)
+
 
 if __name__ == '__main__':
     start(QDRAMA)
