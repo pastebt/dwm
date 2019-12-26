d2e445e5742af2c1a11e08c13b2f476207236b84
pastebt <maye1968@gmail.com>
2019-12-25 21:29:59 -0800

    kantv6.py

--- kantv6.py
+++ kantv6.py
@@ -17,19 +17,28 @@ class KANTV6(DWM):
         # https://www.kantv6.com/index.php/video/play?tvid=301948271219001&part_id=161948271219033&line=1&seo=tvdrama
         hutf = self.chrome_hutf(url)
         title = SelStr("h4.mtg-videoplay-title", hutf)[0].text
+        echo(title)
         title = title.replace("&nbsp;", "_")
+        echo(title)
+        #return
         m = re.search("/(tvdrama)/(\d+)-(\d+)", url)
         sect, tvid, ptid = m.groups()
         du = "https://www.kantv6.com/index.php/video/play"
         du = "%s?tvid=%s&part_id=%s&line=1&seo=%s" % (du, tvid, ptid, sect)
         dat = self.get_hutf(du)
         dat = json.loads(dat)
-        return title, None, [url], None
+        us = self.try_m3u8('https:' + dat['data']['url'])
+        return title, None, us, None
 
     def get_playlist(self, url):
         ns = SelStr('div.entry-content.rich-content tr td a',
                     self.get_hutf(url))
         return [(a.text, a['href']) for a in ns]
+
+    def test(self, argv):
+        url = 'https://www.kantv6.com/tvdrama/301948271219001-161948271219033'
+        hutf = sel.chrome_hutf(url)
+        echo(hutf)
 
 
 if __name__ == '__main__':
