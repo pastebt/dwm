40a76158e432efc152e8b1fb145a3bae8b7873eb
pastebt <maye1968@gmail.com>
2019-12-26 10:05:52 -0800

    修改：     kantv6.py

--- kantv6.py
+++ kantv6.py
@@ -15,16 +15,11 @@ class KANTV6(DWM):
     def query_info(self, url):
         # https://www.kantv6.com/tvdrama/301948271219001-161948271219033
         # https://www.kantv6.com/index.php/video/play?tvid=301948271219001&part_id=161948271219033&line=1&seo=tvdrama
-        #hutf = self.chrome_hutf(url)
-        #title = SelStr("h4.mtg-videoplay-title", hutf)[0].text
-        #echo(title)
-        #title = title.replace("&nbsp;", "_")
-        #echo(title)
-        #return
-        m = re.search("/(tvdrama)/(\d+)-(\d+)", url)
-        sect, tvid, ptid = m.groups()
+        sect, tvid, ptid = self.get_stp(url)
+        if not ptid:
+            echo("no ptid")
+            return
         title = self.get_title(tvid, ptid, sect)
-        #return
         du = "https://www.kantv6.com/index.php/video/play"
         du = "%s?tvid=%s&part_id=%s&line=1&seo=%s" % (du, tvid, ptid, sect)
         dat = self.get_hutf(du)
@@ -37,9 +32,14 @@ class KANTV6(DWM):
         return title, None, us, None
 
     def get_playlist(self, url):
-        ns = SelStr('div.entry-content.rich-content tr td a',
-                    self.get_hutf(url))
-        return [(a.text, a['href']) for a in ns]
+        sect, tvid, ptid = self.get_stp(url)
+        u = 'https://www.kantv6.com/index.php/video/part'
+        u = '%s?tvid=%s' % (u, tvid)
+        dat = self.get_hutf(u)
+        dat = json.loads(dat)
+        debug(json.dumps(dat, indent=2))
+        bu = 'https://www.kantv6.com/%s/%s-' % (sect, tvid)
+        return [(a['part_title'], bu + a['part_id']) for a in dat['data']['partList']]
 
     def get_title(self, tvid, ptid, sect):
         u = "https://www.kantv6.com/index.php/video/info"
@@ -48,25 +48,23 @@ class KANTV6(DWM):
         dat = json.loads(dat)
         debug(json.dumps(dat, indent=2))
         return dat['data']['title']
-    
-    #def get_title_one(self, url):
-    #    hutf = self.chrome_hutf(url)
-    #    #echo(hutf)
-    #    h4 = SelStr("h4.mtg-videoplay-title", hutf)[0]
-    #    t = h4("span")[0].text.strip()
-    #    p = h4("span#cPartNum")[0].text.strip()
-    #    if t and p:
-    #        return t + '_' + p
-    #    return ""
-        
-        #title = title.replace("&nbsp;", "_")
-        #echo(title)
+
+    def get_stp(self, url):
+        m = re.search("/(tvdrama)/(\d+)-(\d+)", url)
+        if m:
+            sect, tvid, ptid = m.groups()
+            return sect, tvid, ptid
+        m = re.search("/(tvdrama)/(\d+)", url)
+        sect, tvid = m.groups()
+        return sect, tvid, ""
 
     def test(self, argv):
         url = 'https://www.kantv6.com/tvdrama/301948271219001-161948271219033'
         url = 'https://www.kantv6.com/index.php/video/part?tvid=301948271219001'
         url = 'https://www.kantv6.com/index.php/video/info?tvid=301948271219001&seo=tvdrama'
-        self.get_title(url)
+        url = 'https://www.kantv6.com/tvdrama/301948271219001'
+        #self.get_title(url)
+        self.get_playlist(url)
 
 
 if __name__ == '__main__':
