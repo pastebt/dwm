05e385692259285fd80e3c585d655ec348d2ceac
pastebt <maye1968@gmail.com>
2019-12-26 12:46:25 -0800

    修改：     kantv6.py

--- kantv6.py
+++ kantv6.py
@@ -13,13 +13,20 @@ class KANTV6(DWM):
     handle_list = ['//kantv6\.com/', '//www\.kantv6\.com/']
 
     def query_info(self, url):
+        title, mu = "", url
+        if 'm3u8' not in url:
+            title, mu = self.get_m3u8(url)
+        us = self.try_m3u8(mu)
+        return title, None, us, None
+
+    def get_m3u8(self, url):
         # https://www.kantv6.com/tvdrama/301948271219001-161948271219033
         # https://www.kantv6.com/index.php/video/play?tvid=301948271219001&part_id=161948271219033&line=1&seo=tvdrama
         sect, tvid, ptid = self.get_stp(url)
         if not ptid:
             echo("no ptid")
             return
-        title = self.get_title(tvid, ptid, sect)
+        title = self.get_title(tvid, sect)
         du = "https://www.kantv6.com/index.php/video/play"
         du = "%s?tvid=%s&part_id=%s&line=1&seo=%s" % (du, tvid, ptid, sect)
         dat = self.get_hutf(du)
@@ -28,20 +35,24 @@ class KANTV6(DWM):
         debug(json.dumps(dat, indent=2))
         echo("title", title)
         #return
-        us = self.try_m3u8('https:' + dat['data']['url'])
-        return title, None, us, None
+        #us = self.try_m3u8('https:' + dat['data']['url'])
+        #return title, None, us, None
+        return title, 'https:' + dat['data']['url']
+
 
     def get_playlist(self, url):
         sect, tvid, ptid = self.get_stp(url)
         u = 'https://www.kantv6.com/index.php/video/part'
         u = '%s?tvid=%s' % (u, tvid)
+        t = self.get_title(tvid, "tvdrama")
         dat = self.get_hutf(u)
         dat = json.loads(dat)
         debug(json.dumps(dat, indent=2))
-        bu = 'https://www.kantv6.com/%s/%s-' % (sect, tvid)
-        return [(a['part_title'], bu + a['part_id']) for a in dat['data']['partList']]
+        #bu = 'https://www.kantv6.com/%s/%s-' % (sect, tvid)
+        debug(t)
+        return [(t + '_' + a['part_title'], "https:" + a['url']) for a in dat['data']['partList']]
 
-    def get_title(self, tvid, ptid, sect):
+    def get_title(self, tvid, sect):
         u = "https://www.kantv6.com/index.php/video/info"
         u = "%s?tvid=%s&seo=%s" % (u, tvid, sect)
         dat = self.get_hutf(u)
@@ -64,7 +75,8 @@ class KANTV6(DWM):
         url = 'https://www.kantv6.com/index.php/video/info?tvid=301948271219001&seo=tvdrama'
         url = 'https://www.kantv6.com/tvdrama/301948271219001'
         #self.get_title(url)
-        self.get_playlist(url)
+        l = self.get_playlist(url)
+        echo(json.dumps(l, indent=2))
 
 
 if __name__ == '__main__':
