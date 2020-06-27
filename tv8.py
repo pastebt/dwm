3a2a16d5f3d4537067732e086e0373f425713b41
pastebt <maye1968@gmail.com>
2020-06-27 15:45:39 -0700

    update tv8

--- tv8.py
+++ tv8.py
@@ -1,8 +1,8 @@
 # -*- coding: utf8 -*-
 
-from subprocess import Popen, PIPE
+import json
 
-from mybs import SelStr
+from mybs import SelStr, DataNode
 from comm import DWM, echo, start, match1
 
 
@@ -10,6 +10,19 @@ class TV8(DWM):
     handle_list = ['/tv8\.fun/', '.dayi.ca/']
 
     def query_info(self, url):
+        hutf = self.get_hutf(url)
+        #obj = match1(hutf, r" var\s+videoObject\s*\=\s*({[^}]+})")
+        #mu = match1(obj, ' video:\s*(\S+)').strip('"')
+        #mu = self.last_m3u8(mu)
+        h = SelStr("h3", hutf)[0]
+        d = SelStr("div.post-entry p", hutf)[0]
+        mu = match1(d.text, ' video:\s*(\S+)').strip('"')
+        mu = self.last_m3u8(mu)
+        d.children = [c for c in d.children if isinstance(c, DataNode)]
+        title = h.text.strip() + " " + d.text.strip()
+        return title, "m3u8", mu, None
+
+    def query_info1(self, url):
         # url = 'http://www.dayi.ca/ys/?p=2386&page=52'
         hutf = self.get_hutf(url)
         # echo(hutf)
@@ -52,7 +65,23 @@ class TV8(DWM):
         # echo(self.get_playlist('http://tv8.fun/20170328-人民的名义/'))
         # 'http://www.dayi.ca/ys/?p=3004&page=2'
         #url = 'http://www.dayi.ca/ys/?p=2386&page=52'
-        url = 'http://www.dayi.ca/ys/?p=3004&page=1'
+        #url = 'http://www.dayi.ca/ys/?p=3004&page=1'
+        url = 'http://www.dayi.ca/ys/?p=4076&&page=1'
+        hutf = self.get_hutf(url)
+        h = SelStr("h3", hutf)[0]
+        echo(h.text)
+        d = SelStr("div.post-entry p", hutf)[0]
+        d.children = [c for c in d.children if isinstance(c, DataNode)]
+        echo(d.text)
+        #dat = match1(hutf, r" var\s+videoObject\s*\=\s*({[^}]+})")
+        #hutf = '''//advertisements:'website:http://dayi.ca/ys/wp-content/themes/responsive-child/ad.json',
+        #        video:"https://videos6.jsyunbf.com/20190630/MnyGwQRx/index.m3u8"
+                
+        #}'''
+        #dat = match1(hutf, ' video:\s*(\S+)')
+        #dat = dat.strip('"')
+        #dat = json.loads(dat)["video"]
+        #echo(dat)
 
 
 if __name__ == '__main__':
