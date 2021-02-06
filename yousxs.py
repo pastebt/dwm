bda67b460e812deef5c004bad28fa8276cf095f2
pastebt <maye1968@gmail.com>
2021-02-06 15:52:52 -0800

    modified:   yousxs.py

--- yousxs.py
+++ yousxs.py
@@ -7,25 +7,37 @@ from mybs import SelStr
 from comm import DWM, echo, start, match1, norm_url
 
 
-class DRAMA8(DWM):
+class YOUSXS(DWM):
     handle_list = ['(/|\.)yousxs\.com(/|:)']
 
     def query_info(self, url):
-        # http://8drama.com/122804/
-        #http://8drama.net/ipobar_.php?sign=251438...
+        #url = "https://www.yousxs.com/player/4659_1.html"
         hutf = self.get_hutf(url)
-        url = SelStr('video source', hutf)[0]['src']
-        title = SelStr('h1.entry-title', hutf)[0].text
-        return title, None, [url], None
+        title = SelStr("h3", hutf)[0].text
+        skey = match1(hutf, "\s+var\s+skey\s*=\s*'(\S+)'\s*;")
+        mp3url = match1(hutf, " '(\S+skey=)'\s*\+\s*skey")
+        u = norm_url(mp3url + skey)
+        return title, "mp3", [u], None
 
     def get_playlist(self, url):
-        ns = SelStr('div.entry-content.rich-content tr td a',
-                    self.get_hutf(url))
-        return [(a.text, a['href']) for a in ns]
-
+        mid = match1(url, "yousxs.com/(\d+).html")
+        hutf = self.get_hutf(url)
+        title = SelStr("h3", hutf)[0].text
+        al = SelStr("div.panel-body div.col-xs-3 a", hutf)
+        ul = []
+        for a in al:
+            n = match1(a['href'], "player/" + mid + "_(\d+).html")
+            if n is None:
+                continue
+            n = int(n)
+            ul.append(("%s_%02d.mp3" % (title, n),
+                       "https://www.yousxs.com/player/%s_%d.html" % (mid, n)))
+        return ul
+       
     def test(self, argv):
-        #url = "https://www.yousxs.com/4659.html"
-        url = "https://www.yousxs.com/player/4659_1.html"
+        url = "https://www.yousxs.com/4659.html"
+        echo(self.get_playlist(url))
+        #url = "https://www.yousxs.com/player/4659_1.html"
         #hutf = self.get_hutf(url)
         #echo(hutf)
         hutf = open("y_4659_1.html").read().decode("utf8")
@@ -35,7 +47,17 @@ class DRAMA8(DWM):
         mp3url = match1(hutf, " '(\S+skey=)'\s*\+\s*skey")
         #echo(quote(mp3url.encode('utf8'), ":?=/"))
         echo(norm_url(mp3url + skey)) #.encode('utf8')))
+        mid = match1(url, "yousxs.com/(\d+).html")
+        echo(mid)
+        hutf = open("y_4659.html").read().decode("utf8")
+        al = SelStr("div.panel-body div.col-xs-3 a", hutf)
+        echo(al)
+        for a in al:
+            n = match1(a['href'], "player/4659_(\d+).html")
+            if n is None:
+                continue
+            echo(n)
 
 
 if __name__ == '__main__':
-    start(DRAMA8)
+    start(YOUSXS)
