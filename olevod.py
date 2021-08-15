bc77b11739c61f7581f5331efcfa0956db7f5678
pastebt <maye1968@gmail.com>
2021-08-15 09:50:48 -0700

    修改：     olevod.py

--- olevod.py
+++ olevod.py
@@ -19,100 +19,58 @@ class OLEVOD(DWM):
     m3u8_slow_merge = True
 
     def query_info(self, url):
-        title, murl = self.title_murl(url)
+        murl = self.find_murl(url)
         murl = self.last_m3u8(murl)
-        echo(murl)
+        debug(murl)
+        title = self.get_title(url)
         return title, "m3u8", murl, None
 
+    def get_title(self, url):
+        hutf = self.get_hutf(url)
+        t = SelStr("div.video_title h2.title", hutf)
+        if t:
+            return t[0].text
+        return None
+
     def get_playlist(self, url):
-        chan, title, keys = self.detail_key(url)
-        if not keys:
-            return []
-        if chan == u'电影':
-            #echo("this is 电影, no playlist")
-            if len(keys) == 1:
-                return [(title, self.key_url(keys[0]))]
-            return [("%s_%02d" % (title, i), self.key_url(k))
-                    for i, k in enumerate(keys, 1)]
-        return [(u"%s_第%02d集" % (title, i), self.key_url(k))
-                for i, k in enumerate(keys, 1)]
+        hutf = self.get_hutf(url)
+        t = SelStr("div.video_title h2.title", hutf)
+        title = "Unknown"
+        if t:
+            title = t[0].text
+        ts = SelStr("div#playlistbox ul.content_playlist li a", hutf)
+        return [(u"%s_%s" % (title, t.text), "https://www.olevod.com" + t['href']) for t in ts]
 
-    def title_murl(self, url):
-        #url = 'https://train.ifvod.tv/detail?id=pGhytibvDFN'
-        #url = 'https://train.ifvod.tv/play?id=p883vn6dt9F'
+    def find_murl(self, url):
+        #url = 'https://www.olevod.com/index.php/vod/play/id/24986/sid/1/nid/76.html'
         ci = get_ci()
-        req_id, murl = "", ""
+        murl = ""
 
-        qnrr = Queue()
-        def nrr(ci, msg):
-            url = msg['params']['response']['url']
-            if '/api/video/play' in url:
-                qnrr.put(msg['params']['requestId'])
-        ci.reg("Network.responseReceived", nrr)
-        
         qnrwb = Queue()
         def nrwb(ci, msg):
             u = msg['params']['request']['url']
             debug("Network.requestWillBeSent url", url)
-            #if 'chunklist.m3u8' in u or ".mp4?" in u:
-            if '.m3u8' in u or ".mp4?" in u:
-                debug("got chunklist.m3u8", u)
+            if 'master.m3u8' in u:
+                debug("got master.m3u8", u)
                 qnrwb.put(u)
-                #sys.exit(1)
         ci.reg("Network.requestWillBeSent", nrwb)
             
         try:
             ci.Page.navigate(url=url)
-            #req_id = qnrr.get(timeout=ci.get_to())
-            #debug("req_id = ", req_id)
-            ##ret = ci.Netw:ork.getResponseBody(requestId=req_id)
-            #ret = ci.wait("Network.getResponseBody", requestId=req_id)
-            #debug("ret_grb =", json.dumps(ret, indent=2))
-            #body = json.loads(ret['result']['body'])
-            #debug("ret_body =", json.dumps(body, indent=2))
-            #zero = body['data']['info'][0]
-            ## find murl, this is not ready one
-            #debug("url = ", zero['flvPathList'][-1]['result'])
-            #title = zero['vl']['title']
-            #debug("title=", title)
             murl = qnrwb.get(timeout=ci.get_to())
             debug("murl = ", murl)
-            title = "123456"
-            return title, murl
+            return murl
         #except Exception as e:
         #    echo("key_m3u8 out:", repr(e))
         finally:
             ci.close()
 
-    def detail_key(self, url):
-        ci = get_ci(debug())
-        qnrr = Queue()
-        def nrr(ci, msg):
-            url = msg['params']['response']['url']
-            if '/api/video/detail' in url:
-                qnrr.put(msg['params']['requestId'])
-        ci.reg("Network.responseReceived", nrr)
- 
-        try:
-            ci.Page.navigate(url=url)
-            req_id = qnrr.get(timeout=ci.get_to())
-            ret = ci.Network.getResponseBody(requestId=req_id)
-            body = json.loads(ret['result']['body'])
-            bdi0 = body['data']['info'][0]
-            title, channel = bdi0['title'], bdi0['channel']
-            keys = [g['key'] for g in bdi0['guestSeriesList']]
-            debug("keys = ", keys)
-            return channel, title, keys
-        except Exception as e:
-            echo("detail_key out:", repr(e))
-        finally:
-            ci.close()
-        return None, None, []
-
     def test(self, args):
         url = "https://www.olevod.com/index.php/vod/play/id/24986/sid/1/nid/75.html"
-        hutf = self.get_hutf(url)
-        echo(hutf)
+        for t in (self.get_playlist(url)):
+            echo(t[0], t[1])
+        #hutf = self.get_hutf(url)
+        #echo(hutf)
         #self.title_murl(url)
         # https://europe.olemovienews.com/hlstimeofffmp4/20210503/xyflmsiG/mp4/xyflmsiG.mp4/master.m3u8
 
