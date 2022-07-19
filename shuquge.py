08602d6b28c6bd5517ecbd465b33eac424fced34
pastebt <maye1968@gmail.com>
2022-07-18 17:22:58 -0700

    modified:   comm.py modified:   shuquge.py

--- shuquge.py
+++ shuquge.py
@@ -9,31 +9,43 @@ from comm import DWM, echo, start, match1
 
 
 class SHUQUGE(DWM):
-    handle_list = ['shuduge.com']
+    handle_list = ['shuquge.com']
 
     def query_info(self, url):
-        # http://m.bookdown.com.cn/read/31314_97_2.html
+        #url = "https://www.shuquge.com/txt/12236/46252712.html"
         hutf = self.get_hutf(url)
-        echo(hutf)
-        us = self._get_m3u8_urls(url, hutf)
+        #echo(hutf)
+        ct = SelStr("div.content", hutf)
+        #echo(ct.text)
+        t = SelStr("div.content h1", hutf)[0]
         #return "", "mp4", us, None
-        return "", None, us, None
+        return t.text, "epub", ct, 1
 
-    def try_playlist(self, url):
+    def save_book(self, outfn, ct):
+        #echo(outfn)
+        with open(outfn, "w") as fout:
+            dat = unicode(ct[0])
+            fout.write(dat.encode('utf8'))
+
+    def get_playlist(self, url):
         #url = "https://www.shuquge.com/txt/12236/index.html"
         base = os.path.dirname(url)
         hutf = self.get_hutf(url)
         #hutf = open("s.html").read().decode('utf8')
         #echo(hutf)
         tt = SelStr("div.book div.info h2", hutf)
+        if not tt:
+            return []
         #echo(tt[0].text)
         title = tt[0].text
         echo(title)
-        ul = SelStr("div.listmain dl", hutf)[0]
+        ul = SelStr("div.listmain dl", hutf)
+        if not ul:
+            return []
         #for u in ul.descendants:
         sel = True
         lst = []
-        for u in ul.children:
+        for u in ul[0].children:
             #echo(u)
             if u.tag == 'dt':
                 sel =  u"最新章节" not in u.text
@@ -50,7 +62,7 @@ class SHUQUGE(DWM):
         hutf = open("s1.html").read().decode('utf8')
         #hutf = self.get_hutf(url)
         #echo(hutf)
-        ct = SelStr("div.content", hutf)[0]
+        #ct = SelStr("div.content", hutf)[0]
         echo(ct.text)
     
 
